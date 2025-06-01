8132364c95ab319f24b16276f10d07bb4f4a7275
Allan1496 <allansantiago833@gmail.com>
2025-06-01 16:33:56 -0400

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('Audit Tracker')),
+      body: Center(child: Text('Hello from your Audit App')),
+    ),
+  ));
+}
